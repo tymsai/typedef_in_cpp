e0a320f63d1058e519072a40e3c46c7a34ed8357
T.Y.M.SAI <tymsai6076@gmail.com>
2021-09-29 17:18:27 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+typedef int newname;
+int main(){
+    newname a;
+    cout<<"This program shows the uses of typedef keyword\n";
+    cin>>a;
+    cout<<"Here the data type name of variable, is changed to a different name , and uses it to identify the data type of the variable \n";
+    cout<<a;
+    return 0;
+}
